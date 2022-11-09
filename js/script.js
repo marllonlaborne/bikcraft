c2e97bd8f1b462c55244db9eb16b60b5fa716c77
Marllon <99225101+marllonlaborne@users.noreply.github.com>
2022-11-08 21:37:59 -0300

    interações com bicicletas

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function ativarPergunta(event) {
   const controls = pergunta.getAttribute('aria-controls');
   const resposta = document.getElementById(controls);
   
-  resposta.classList.toggle('ativo');
+  resposta.classList.toggle('ativo');g
   const ativa = resposta.classList.contains('ativo');
   pergunta.setAttribute('aria-expanded', ativa);
 }
@@ -42,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 1000px)').matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria)
